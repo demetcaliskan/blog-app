e125271e3aa106a710d243e8537fb7b6859d29ce
Demet Çalışkan <demet@aposto.com>
2023-04-05 01:15:03 +0300

    add blog context

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { Provider } from './src/context/BlogContext'
+
+import IndexScreen from './src/screens/IndexScreen'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerTitle: 'Blogs' }}>
+          <Stack.Screen name='Index' component={IndexScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
