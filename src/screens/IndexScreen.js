bbfdc9f3ec5c8a437612588e9c9b0e252cbe2103
Demet Çalışkan <demet@aposto.com>
2023-04-05 01:33:14 +0300

    add delete blog post function

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,45 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useContext } from 'react'
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+import { Context } from '../context/BlogContext'
+import { Feather } from '@expo/vector-icons'
 
 const IndexScreen = () => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context)
   return (
     <>
-      <Text>Index Screen</Text>
+      <Button title='Add Post' onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.id}
+        renderItem={({ item }) => (
+          <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+              <Feather style={styles.icon} name='trash' />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray',
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+})
 
 export default IndexScreen
